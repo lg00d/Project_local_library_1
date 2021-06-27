53d6a8ad49fbb2bea21776a6b113c70e0b0b2651
lg00d <egoodman1180@icloud.com>
2021-06-27 11:00:42 -0400

    cereted sortAccountsByLastName function

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -2,7 +2,9 @@ function findAccountById(accounts, id) {
   return accounts.find((account) => account.id === id);
 }
 
-function sortAccountsByLastName(accounts) { }
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1)
+}
 
 function getTotalNumberOfBorrows(account, books) { }
 
