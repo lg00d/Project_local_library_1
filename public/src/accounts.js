3741fe411cb52deb97d44fcce4326c1495e8805a
lg00d <egoodman1180@icloud.com>
2021-06-27 20:42:31 -0400

    created findAuthorById function

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -6,9 +6,43 @@ function sortAccountsByLastName(accounts) {
   return accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
 }
 
-function getTotalNumberOfBorrows(account, books) { }
+function getTotalNumberOfBorrows(account, books) {
+  const { id } = account;
+  let total = 0;
 
-function getBooksPossessedByAccount(account, books, authors) { }
+  for (let book in books) {
+    const { borrows } = books[book];
+    borrows.forEach((element) => {
+      if (element.id === id) {
+        total++;
+      }
+    });
+  }
+
+  return total;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const accountId = account.id;
+
+  let result = [];
+
+  result = books.filter((book) => {
+    return book.borrows.some((borrow) => borrow.id === accountId && !borrow.returned);
+  });
+
+  result = result.map((book) => {
+    const author = getAuthorById(authors, book.authorId);
+    const newBook = {
+      ...book,
+      author,
+    };
+
+    return newBook;
+  });
+
+  return result;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,12 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) { }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) { }
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) { }
 
 module.exports = {
   findAuthorById,
